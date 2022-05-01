9fd0424038aa93cd94a810e6062f140d098e7e95
gaurabdhg <77386673+gaurabdhg@users.noreply.github.com>
2022-05-01 14:40:42 +0200

    Update scraper linkedin.py

--- scraper linkedin.py
+++ scraper linkedin.py
@@ -1,13 +1,14 @@
 import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 def searching():
-    from selenium import webdriver
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.wait import WebDriverWait
-    from selenium.webdriver.common.action_chains import ActionChains
-
+    
 
     driver=webdriver.Chrome("C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe")
     driver.get("https://www.linkedin.com")
